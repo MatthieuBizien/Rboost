140d080d1e860675e07332300f20b304f4132699
Matthieu Bizien <matthieu@databiz.io>
2018-10-23 17:36:28 +0200

    MBZ: save model and prediction in example1

--- rboost/examples/example1.rs
+++ rboost/examples/example1.rs
@@ -1,10 +1,14 @@
 extern crate cpuprofiler;
+extern crate csv;
 extern crate failure;
 extern crate rboost;
+extern crate serde_json;
 
 use cpuprofiler::PROFILER;
 use failure::Error;
 use rboost::{ColumnMajorMatrix, Dataset, Params, GBT};
+use std::fs::File;
+use std::io::Write;
 
 fn parse_tsv(data: &str) -> Result<Dataset, Error> {
     let mut target: Vec<f64> = Vec::new();
@@ -40,20 +44,45 @@ fn main() {
     let params = Params {
         gamma: 0.,
         lambda: 1.,
-        learning_rate: 0.8,
+        learning_rate: 0.99,
         max_depth: 3,
         min_split_gain: 0.1,
-        n_bins: 255,
+        n_bins: 0,
     };
-    PROFILER.lock().unwrap().start("./my-prof.profile").unwrap();
-    let gbt = GBT::build(&params, &train, 100, Some(&test), 100);
+
+    println!("Profiling to example1.profile");
+    PROFILER.lock().unwrap().start("./example1.profile").unwrap();
+    let gbt = GBT::build(&params, &train, 1000, Some(&test), 1000);
     PROFILER.lock().unwrap().stop().unwrap();
-    let yhat: Vec<f64> = (0..train.features.n_rows())
+
+    let yhat_train: Vec<f64> = (0..train.features.n_rows())
         .map(|i| gbt.predict(&train.row(i)))
         .collect();
-    println!("RMSE train {:.8}", rmse(&train.target, &yhat));
-    let yhat: Vec<f64> = (0..test.features.n_rows())
+    println!("RMSE train {:.8}", rmse(&train.target, &yhat_train));
+
+    let yhat_test: Vec<f64> = (0..test.features.n_rows())
         .map(|i| gbt.predict(&test.row(i)))
         .collect();
-    println!("RMSE Test {:.8}", rmse(&test.target, &yhat));
+    println!("RMSE Test {:.8}", rmse(&test.target, &yhat_test));
+
+    println!("Serializing model to example1.json");
+    let serialized: String = serde_json::to_string(&gbt).expect("Error on JSON serialization");
+    let mut file = File::create("example1.json").expect("Error on file creation");
+    file.write_all(serialized.as_bytes())
+        .expect("Error on writing of the JSON");
+
+    println!("Writing predictions to example1.csv");
+    let file = File::create("example1.csv").expect("Error on file creation");
+    let mut wtr = csv::Writer::from_writer(file);
+    wtr.write_record(&["dataset", "true_val", "yhat"])
+        .expect("Error on csv writing");
+    for (true_val, yhat) in train.target.iter().zip(yhat_train.iter()) {
+        wtr.write_record(&["train", &true_val.to_string(), &yhat.to_string()])
+            .expect("Error on csv writing");
+    }
+    for (true_val, yhat) in test.target.iter().zip(yhat_test.iter()) {
+        wtr.write_record(&["test", &true_val.to_string(), &yhat.to_string()])
+            .expect("Error on csv writing");
+    }
+    wtr.flush().expect("Error on CSV flushing");
 }

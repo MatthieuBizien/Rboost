b18a82c9f232a938e5f287561a83f1166c21a925
Matthieu Bizien <matthieu@databiz.io>
2018-12-27 00:38:33 +0100

    MBZ: add example example_tree

---
+++ rboost/examples/example_tree.rs
@@ -0,0 +1,48 @@
+#![feature(duration_as_u128)]
+
+extern crate cpuprofiler;
+extern crate csv;
+extern crate rboost;
+extern crate serde_json;
+
+use rboost::{parse_csv, rmse, BinaryLogLoss, DecisionTree, TreeParams};
+
+fn main() -> Result<(), Box<::std::error::Error>> {
+    // Load the data
+    let train = include_str!("../data/regression.train");
+    let train = parse_csv(train, "\t")?;
+    let test = include_str!("../data/regression.test");
+    let test = parse_csv(test, "\t")?;
+
+    let n_bins = 128;
+
+    for max_depth in 0..8 {
+        let tree_params = TreeParams {
+            gamma: 0.,
+            lambda: 1.,
+            max_depth,
+            min_split_gain: 0.,
+        };
+        println!("\nParams tree{:?} n_bins={}", tree_params, n_bins);
+
+        let mut predictions = vec![0.; train.target.len()];
+        let tree = DecisionTree::build(
+            &mut train.as_prepared_data(n_bins)?,
+            &mut predictions,
+            &tree_params,
+            BinaryLogLoss::default(),
+        )?;
+
+        let yhat_train: Vec<f64> = (0..train.features.n_rows())
+            .map(|i| tree.predict(&train.features.row(i)))
+            .collect();
+        println!("RMSE train {:.8}", rmse(&train.target, &yhat_train));
+
+        let yhat_test: Vec<f64> = (0..test.features.n_rows())
+            .map(|i| tree.predict(&test.features.row(i)))
+            .collect();
+        println!("RMSE Test {:.8}", rmse(&test.target, &yhat_test));
+    }
+
+    Ok(())
+}
